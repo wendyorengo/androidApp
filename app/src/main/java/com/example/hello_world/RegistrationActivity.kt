e582e5ac5899fd3e96b39f1d5c2e0b25a9442a27
MercyRima <macrimah60@gmail.com>
2020-07-21 20:30:03 +0300

    register and login layouts

---
+++ app/src/main/java/com/example/hello_world/RegistrationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hello_world
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
