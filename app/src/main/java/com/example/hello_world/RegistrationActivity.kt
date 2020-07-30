f5a338f626726027141e31a76b1f61b55db5a9c4
MercyRima <macrimah60@gmail.com>
2020-07-30 22:18:22 +0300

    register and login layouts

--- app/src/main/java/com/example/hello_world/RegistrationActivity.kt
+++ app/src/main/java/com/example/hello_world/RegistrationActivity.kt
@@ -1,11 +1,30 @@
 package com.example.hello_world
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_registration.*
+import kotlinx.android.synthetic.main.activity_registration.view.*
 
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        button2.setOnClickListener{
+            val intent = Intent(baseContext,MainActivity::class.java)
+            startActivity(intent)
+            var firstName = textView2.text.toString()
+            var lastName = textView3.text.toString()
+            var email = textView4.text.toString()
+            var password = editTextPassword.text
+            var passwordConfirm = textView5.text.toString()
+            var forgotPassword = textViewForgotPassword.text.toString()
     }
+
+
+
+    }
+
 }
+
